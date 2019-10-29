957c4e441e0aa9fcc3368bda3f8e1e8584ecf952
Jibram <cajibram@gmail.com>
2019-10-29 12:38:26 -0700

    Scaffolding

---
+++ src/components/Test.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Test extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    
+
+    render() {
+        return(
+            <div>
+                This is where we want the animation
+            </div>
+        )
+    }
+
+}
+
+export default Test
