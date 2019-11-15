e2ef611b4b92c605f7b1ab2b0933e82e7d82addb
Hoa <hnguyen257@ucmerced.edu>
2019-11-15 11:24:31 -0800

    lab4.3, tried to add webgzer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import LandingPage from './components/LandingPage'
 import Test from './components/Test'
 import AdminPage from './components/AdminPage'
 
+//Webgazer (Hoa)
+//import webgazer from './webgazer';
+//import main from './main';
+
 class App extends React.Component {
   constructor(props){
     super(props)
@@ -44,5 +48,37 @@ class App extends React.Component {
     )
   }
 }
+
+window.onload = function() {
+  //start the webgazer tracker
+  webgazer.setRegression('ridge') /* currently must set regression and tracker */
+      .setTracker('clmtrackr')
+      .setGazeListener(function(data, clock) {
+           console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+           console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+      })
+      .begin()
+      .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+
+
+  //Set up the webgazer video feedback.
+  var setup = function() {
+
+      //Set up the main canvas. The main canvas is used to calibrate the webgazer.
+      var canvas = document.getElementById("plotting_canvas");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      canvas.style.position = 'fixed';
+  };
+
+  function checkIfReady() {
+      if (webgazer.isReady()) {
+          setup();
+      } else {
+          setTimeout(checkIfReady, 100);
+      }
+  }
+  setTimeout(checkIfReady,100);
+};
 
 export default App;
