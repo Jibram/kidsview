c6284b9b782ae68fcecf4bceac5a147c0fa7873a
Raj Yuvaraj <ryuvaraj37@gmail.com>
2019-11-21 02:05:35 -0800

    Exam description and basic functionality
    Added a start screen for VA test that explains the test and start it
    then the screen goes to a simple screen with a letter, textbox, and one button to end the test.
    after "e" the letters are random press 1 for correct anything else for wrong.
    Now i need to chance the sizes of the letters as the test progresses.

--- src/components/VisualAcuity.js
+++ src/components/VisualAcuity.js
@@ -5,37 +5,63 @@ import './VisualAcuity.css'
 class VisualAcuity extends React.Component {
     constructor(props){
         super(props)
-        this.board = {
-            letter : 'E',
-            accuracy : 0
+        this.state = {
+            letter : 'e',
+            accuracy : 0,
+            level : 0,
+            amount : 0,
         }
-        this.round = {
-            level : 1,
-            amount : 1,
-            size : 300 
-        }
+        this.handleStartTest = this.handleStartTest.bind(this)
+        this.handleKeyCheck = this.handleKeyCheck.bind(this)
+        this.getRandomLetter = this.getRandomLetter.bind(this)
 
     }
 
+    handleStartTest(e) {
+        this.setState({level:1});
+    }
+
+    handleKeyCheck(e){
+
+        
+        if (e.key == 1) {
+            this.setState({accuracy:this.state.accuracy+1});
+        }
+
+        this.setState({amount:this.state.amount+1});
+        this.setState({letter:this.getRandomLetter()});
+        console.log(this.state.accuracy + ',' + this.state.amount);
+    }
+
+    getRandomLetter() {
+        return String.fromCharCode(Math.floor(Math.random() * (122 - 97 + 1)) + 97);
+    }
+
     viewSelect() {
-        switch(this.round.level) {
+        switch(this.state.level) {
+                case 0:
+                    return (
+                        <div className='flex'>
+                            <img src={logo} alt="Children's valley hospital" height='100%' width='100%'/>
+                            <h1>Visual Acuity Exam</h1>
+                            <p className='info'>
+                            This exam will test your visual accuracy by simulating a Snellen Eye Chart Exam.<br/><br/>
+                            Please sit one foot away from the screen, and have the test proctor type 1 for correct letters.
+                            </p>
+                            <button type='button' onClick={this.handleStartTest}>Begin Visual Acuity Exam</button>
+                        </div>
+                    )
+
                 case 1:
                     return (
-                        <div className='vatestflex'>
-                            <img src={logo} alt="Children's valley hospital" height='50%' width='100%'/>
-                            <h1 className="round1"><pre>{JSON.stringify(this.board.letter)}</pre></h1>
+                        <div className='flex'>
+                            <h2>{JSON.stringify(this.state.letter)}</h2>
+                            <input type="text" id="one" onKeyPress={this.handleKeyCheck}/>
                             <button type='button' onClick={this.props.handleRestart}>Back to Beginning</button>
                             <div className='separator'></div>
                             <button type='button' onClick={this.props.handleEnd}>End Test</button>
                         </div>
                     )
-                case 2:
-                    
-                    break;
-                case 3:
-                    
-                    break;
-
         }
     }
     
