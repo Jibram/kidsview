5ffc2e3b3dc8de71541badd0f5b8e534ac45d41a
Raj Yuvaraj <ryuvaraj37@gmail.com>
2019-12-05 17:26:52 -0800

    Doctor login functional

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -21,6 +21,7 @@ class AdminPage extends React.Component {
         this.url = "http://localhost:5000"
         this.getPatientById = "/getpatientbyid?patientID="
         this.getPatientByName = "/getpatientbyname?patientName="
+        this.authorizeLogin = "/authorizelogin?userID="
         this.handleLogin = this.handleLogin.bind(this)
         this.handleViewData = this.handleViewData.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
@@ -32,12 +33,19 @@ class AdminPage extends React.Component {
             link = this.url.concat(this.getPatientById, p.toString(10));
         } else if (i == 2) {
             link = this.url.concat(this.getPatientByName, p);
-        } 
+        } else if (i == 3) {
+            link = this.url.concat(this.authorizeLogin, p);
+        }
         var xhr = new XMLHttpRequest();
         var data = null; 
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                data = JSON.parse(xhr.response);
+                if (i > 2) {
+                    data = xhr.response;
+                } else {
+                    data = JSON.parse(xhr.response);
+                }
+                
             }
         };
         xhr.open("GET", link, false);
@@ -46,7 +54,27 @@ class AdminPage extends React.Component {
     }
 
     handleLogin(e) {
-        this.setState({loggedIn:true});
+
+        var userName = document.getElementById('userNameIn');
+        var pw = document.getElementById('pwIn');
+        var data;
+        if (userName.value != '' || pw.value != '') {
+            data = this.handleGetRequests(userName.value,3);
+
+            if (data == pw.value) {
+                this.setState({loggedIn:true});
+            } else {
+                alert('Invalide Username or Password. Try again!')
+            }
+            userName.value = '';
+            pw.value = '';
+            
+        } else {
+            alert('Enter a Username or Password.');
+        }
+
+
+        
     }
 
     handleViewData(e) {
@@ -120,8 +148,8 @@ class AdminPage extends React.Component {
                     <img src={logo} alt="Children's valley hospital" height='100%' width='100%'/>
                     <h1>Administrator Sign In</h1>
                     <div>
-                        <label type='text'>Username <input></input></label><br/>
-                        <label type='password'>Password <input></input></label>
+                        <label type='text'>Username <input id="userNameIn"></input></label><br/>
+                        <label type='password'>Password <input id="pwIn"></input></label>
                     </div>
                     <button type='button' onClick={this.handleLogin}>Log In</button>
                 </div>
